4a9832a957dc76e1ade8e977f2b80100bb7935bf
aleon <aleon@e201-ThinkCentre-M71z>
2014-09-01 19:39:50 -0500

    Test finished

--- src/main/java/co/edu/uelbosque/controller/ImageManager.java
+++ src/main/java/co/edu/uelbosque/controller/ImageManager.java
@@ -4,18 +4,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.Collections;
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author aleon
@@ -23,45 +17,42 @@ import java.util.logging.Logger;
 class ImageManager {
 
     private static ImageManager singleton;
-    private static Boolean control=true;
-    
-    ByteBuffer bb;
-    
-    private ImageManager(){
-       
+    private static Boolean control = true;
+    ByteArrayOutputStream bo;
+
+    private ImageManager(File f) throws IOException {
+
+        bo = new ByteArrayOutputStream();
+        int x;
+        FileInputStream fin = new FileInputStream(f);
         
-        
-        
-      
-    }
-    
-    /**
-     * Imagen almacenada en memoria
-     * @return 
-     */
-    protected byte[] getImage(File f) throws IOException{
-        
-          ByteArrayOutputStream bo=new ByteArrayOutputStream();
-            FileInputStream fin=new FileInputStream(f);
-            int x;
-            
-            while((x=fin.read())!=-1){
-              bo.write(x);
-            }  
-            return bo.toByteArray();
+        while ((x = fin.read()) != -1) {
+            bo.write(x);
+        }
 
     }
-    
-    public static ImageManager getInstance() throws InterruptedException  {
-        if(singleton==null){            
-            synchronized(control){
-             if(singleton!=null){ 
-                 return singleton;
-             }
-             singleton=new ImageManager();
+
+    /**
+     * Imagen almacenada en memoria
+     *
+     * @return
+     */
+    protected byte[] getImage() throws IOException {
+
+        return bo.toByteArray();
+
+    }
+
+    public static ImageManager getInstance(File f) throws InterruptedException, IOException {
+        if (singleton == null) {
+            synchronized (control) {
+                if (singleton != null) {
+                    return singleton;
+                }
+                singleton = new ImageManager(f);
             }
         }
         return singleton;
     }
-    
+
 }
--- src/test/java/co/edu/uelbosque/controller/TestFileManager.java
+++ src/test/java/co/edu/uelbosque/controller/TestFileManager.java
@@ -1,14 +1,10 @@
 package co.edu.uelbosque.controller;
 
 
-import co.edu.uelbosque.controller.ImageManager;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
 import java.util.Random;
-import java.util.logging.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -28,22 +24,21 @@ public class TestFileManager {
     
     static ImageManager unique;
     @Test(threadPoolSize = 100, invocationCount = 100)
-    public void verificaQuelasInstanciasSenIguales() throws InterruptedException{
-        System.out.println(" "+Thread.currentThread().getId());
+    public void verificaQuelasInstanciasSenIguales() throws InterruptedException, IOException, URISyntaxException{
+        System.out.println("Thread Number: "+Thread.currentThread().getId());
         Random r=new Random(System.currentTimeMillis());
             Thread.sleep(r.nextInt(5000));
-        ImageManager img=ImageManager.getInstance(); 
+        File f=new File(this.getClass().getResource("/108.jpg").toURI());
+        ImageManager img=ImageManager.getInstance(f); 
         if(unique==null) unique=img;
         Assert.assertTrue(unique==img);        
     }
     
     @Test
     public void debeCrearUnBufferDeLaImagen() throws InterruptedException, URISyntaxException, IOException{
-        ImageManager im=ImageManager.getInstance();
-        File f=new File(this.getClass().getResource("/BigImageMix.png").toURI());
-        
-        byte x[]=im.getImage(f);
-        System.out.println("X: "+x.length );
-        //Assert.assertNotNull(in);
+        File f=new File(this.getClass().getResource("/108.jpg").toURI());        
+        ImageManager im=ImageManager.getInstance(f);       
+        byte x[]=im.getImage();
+        Assert.assertEquals(x.length,105898);
     }
 }
